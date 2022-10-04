b0431555840e7de319dedef99dd1e65a7fc2c064
Roland Mikołajczak <rolandmikolajczak@icloud.com>
2022-10-04 19:48:53 +0200

    HelloWorld

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World!");
+        System.out.println("Roland");
+        System.out.println("36");
+        System.out.println("R");// [2]
+    }
+}
