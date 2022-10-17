421820d3a240beeaf3cf59caddb4bf3a4c34e886
Roland Mikołajczak <rolandmikolajczak@icloud.com>
2022-10-17 22:20:01 +0200

    Notebook

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,6 +1,20 @@
+import com.kodilla.Notebook;
+
 public class FirstClass {                           // [1]
     public static void main(String[] args) {
-        Notebook notebook = new Notebook// [2]
-        System.out.println("Hello from FirstClass!"); // [3]
+        Notebook notebook = new Notebook(600,1000);
+        System.out.println("notebook.weight: " + notebook.weight);
+        System.out.println("notebook.price: " + notebook.price);
+        notebook.checkPrice();
+        Notebook heavyNotebook = new Notebook(2000,1500);
+        System.out.println("notebook.weight: " + heavyNotebook.weight);
+        System.out.println("notebook.price: " + heavyNotebook.price);
+        heavyNotebook.checkPrice();
+        Notebook oldNotebook = new Notebook(1200,500);
+        System.out.println("notebook.weight: " + oldNotebook.weight);
+        System.out.println("notebook.price: " + oldNotebook.price);
+        oldNotebook.checkPrice();
+
+        }
     }
-}
+
--- kodilla-intro/src/main/java/com/kodilla/Notebook.java
+++ kodilla-intro/src/main/java/com/kodilla/Notebook.java
@@ -1,6 +1,18 @@
 package com.kodilla;
 
 public class Notebook {
-    int weight = 600;
+    public int weight;
+    public int price;
+   public Notebook (int weight, int price) {
+     this.weight = weight;
+     this.price = price;}
+
+       public void checkPrice() {
+           if (this.price < 900) {
+               System.out.println("This notebook is cheap.");
+           } else {
+               System.out.println("This notebook is quite expensive.");}
+
+   }
 
 }
