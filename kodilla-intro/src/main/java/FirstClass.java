c27dd4ee5108818f1d8f19da7fc315267fd8ed48
Roland Mikołajczak <rolandmikolajczak@icloud.com>
2022-10-18 22:44:00 +0200

    checkYear

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -2,21 +2,27 @@ import com.kodilla.Notebook;
 
 public class FirstClass {                           // [1]
     public static void main(String[] args) {
-        Notebook notebook = new Notebook(600,1000);
+        Notebook notebook = new Notebook(600,1000, 2019);
         System.out.println("notebook.weight: " + notebook.weight);
         System.out.println("notebook.price: " + notebook.price);
+        System.out.println("notebook.year: " + notebook.year);
         notebook.checkPrice();
         notebook.checkWeight();
-        Notebook heavyNotebook = new Notebook(2000,1500);
+        notebook.checkYear();
+        Notebook heavyNotebook = new Notebook(2000,1500, 2020);
         System.out.println("notebook.weight: " + heavyNotebook.weight);
         System.out.println("notebook.price: " + heavyNotebook.price);
+        System.out.println("notebook.year: " + heavyNotebook.year);
         heavyNotebook.checkPrice();
         heavyNotebook.checkWeight();
-        Notebook oldNotebook = new Notebook(1200,500);
+        heavyNotebook.checkYear();
+        Notebook oldNotebook = new Notebook(1200,600, 2009);
         System.out.println("notebook.weight: " + oldNotebook.weight);
         System.out.println("notebook.price: " + oldNotebook.price);
+        System.out.println("notebook.year: " + oldNotebook.year);
         oldNotebook.checkPrice();
         oldNotebook.checkWeight();
+        oldNotebook.checkYear();
 
         }
     }
--- kodilla-intro/src/main/java/com/kodilla/Notebook.java
+++ kodilla-intro/src/main/java/com/kodilla/Notebook.java
@@ -3,29 +3,52 @@ package com.kodilla;
 public class Notebook {
     public int weight;
     public int price;
-   public Notebook (int weight, int price) {
-     this.weight = weight;
-     this.price = price;
-   }
 
-       public void checkPrice() {
-           if (this.price < 600) {
-               System.out.println("This notebook is very cheap.");
-           } else if (this.price > 600 && this.price <= 1000) {
-               System.out.println("This notebook is quite expensive.");
-           } else if (this.price > 1000) {
-               System.out.println("This notebook is expensvie");
-           }
+    public int year;
 
-           }
-           public void checkWeight() {
-       if (this.weight <= 600){
-           System.out.println("This notebook is light");
-       } else if (this.weight > 600 && this.weight<= 1200) {
-           System.out.println("This notebook is quite heavy");}
-       else if (this.weight > 1200){
-           System.out.println("This notebook is heavy");}
-               }
+    public Notebook(int weight, int price, int year) {
+        this.weight = weight;
+        this.price = price;
+        this.year = year;
+        System.out.println();
+    }
 
-       }
+    public void checkPrice() {
+        if (this.price < 600) {
+            System.out.println("This notebook is very cheap.");
+        } else if (this.price > 600 && this.price <= 1000) {
+            System.out.println("This notebook is quite expensive.");
+        } else if (this.price > 1000) {
+            System.out.println("This notebook is expensive");
+        }
+
+    }
+
+    public void checkWeight() {
+        if (this.weight <= 600) {
+            System.out.println("This notebook is light");
+        } else if (this.weight > 600 && this.weight <= 1200) {
+            System.out.println("This notebook is quite heavy");
+        } else if (this.weight > 1200) {
+            System.out.println("This notebook is heavy");
+        }
+
+    }
+
+    public void checkYear() {
+        if (this.year <= 2009 & this.price <=600) {
+            System.out.println("This Notebook is old and cheap");
+        } else if (this.year > 2010 && this.year <= 2019 & this.price > 600 && this.price <= 1000) {
+            System.out.println("This notebook is pretty new and in good price");
+        } else if (this.year > 2019 & this.price > 1000) {
+            System.out.println("This notebook is new and quite expensive");
+        }
+    }
+
+
+
+
+
+
+}
 
