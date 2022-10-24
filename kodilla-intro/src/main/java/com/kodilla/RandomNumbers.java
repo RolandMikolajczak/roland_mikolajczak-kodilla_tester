9fae9d3522631efb47c269e2791ffb9452ba0c45
Roland Mikołajczak <rolandmikolajczak@icloud.com>
2022-10-24 03:30:38 +0200

    losowanie

--- kodilla-intro/src/main/java/com/kodilla/RandomNumbers.java
+++ kodilla-intro/src/main/java/com/kodilla/RandomNumbers.java
@@ -1,18 +1,54 @@
 package com.kodilla;
 
-public class RandomNumbers {
-    int RandomNumbers = randomNumbers(5000);
-    int Random;
+import java.util.Random;
 
-    public int randomNumbers(int max) {
-        Random random = new Random();
-        int result = 0;
+public class RandomNumbers {
+    Random random = new Random();
+
+    private int min;
+    private int max;
+    int maximum = 5000;
+    int result = 0;
+
+    public void losowanie() {
         int sum = 0;
-        while (sum < max) {
-            int temp = random.;
+        int temp = 0;
+        this.min = 30;
+        this.max = 0;
+        while (sum < maximum) {
+            temp = random.nextInt(31);
             sum = sum + temp;
-            result++;
+
+            if (temp > this.max) {
+                this.max = temp;
+            }
+            if (temp < this.min) {
+                this.min = temp;
+            }
         }
-        return result;
+    }
+    public int getMax() {
+        return this.max;
+    }
+
+    public int getMin() {
+        return this.min;
+    }
+
+
+    public static void main(String[] args) {
+        RandomNumbers losowanie = new RandomNumbers();
+        losowanie.losowanie();
+        System.out.println("Min: " + losowanie.getMin());
+        System.out.println("Max: " + losowanie.getMax());
     }
 }
+
+
+
+
+
+
+
+
+
