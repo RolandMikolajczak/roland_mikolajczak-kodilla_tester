7c358cb80cc31af31859f2f25b35a45d656192de
Roland Mikołajczak <rolandmikolajczak@icloud.com>
2023-02-01 23:15:02 +0100

    car

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarConfig.java
@@ -0,0 +1,25 @@
+package com.kodilla.spring.basic.spring_configuration.homework;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.MonthDay;
+@Configuration
+public class CarConfig {
+    @Bean
+    public Car dependsOfSeason() {
+        Car car;
+        if ((MonthDay.now().isAfter(MonthDay.of(4, 1))
+                && MonthDay.now().isBefore(MonthDay.of(5, 30)))
+                || (MonthDay.now().isAfter(MonthDay.of(9, 1))
+                && MonthDay.now().isBefore(MonthDay.of(11, 30)))) {
+            car = new Sedan();
+        } else if (MonthDay.now().isAfter(MonthDay.of(12, 1))
+                && MonthDay.now().isBefore(MonthDay.of(3, 31))) {
+            car = new SUV();
+        } else {
+            car = new Cabrio();
+        }
+        return car;
+    }
+}
